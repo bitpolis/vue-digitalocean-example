c9458016204ef79442a1b6a0b68c32958dafb062
bitpolis <oamaur@gmail.com>
2024-06-25 19:52:20 -0500

    primer commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <div v-for="post of posts" v-bind:key="post.id">
+      <h6> {{ post.title }} </h6>
+      <p> {{ post.body }} </p>
+
+  </div>
+  </div>
+</template>
+
+<script>
+const URI = 'https://jsonplaceholder.typicode.com/posts';
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  created() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts() {
+      const response = await fetch(URI);
+      const data = await response.json();
+      this.posts = data;
+  }
+}
+}
+
+</script>
